0497f42c7d8aa16acb77bd463806cdd4151adff4
Ngoun-gif <khemhengnguon@gmail.com>
2025-09-07 09:30:19 +0700

    first commit

---
+++ src/main/java/Demon/St15/St15Application.java
@@ -0,0 +1,13 @@
+package Demon.St15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class St15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(St15Application.class, args);
+	}
+
+}
---
+++ src/test/java/Demon/St15/St15ApplicationTests.java
@@ -0,0 +1,13 @@
+package Demon.St15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class St15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
